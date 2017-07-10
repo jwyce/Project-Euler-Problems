db4500455742b4652112f904db1037fa0f922322
Jared Wyce <wycejared@gmail.com>
2017-07-10 19:03:36 -0400

    ported year old c++ solution

---
+++ Problem_017.java
@@ -0,0 +1,82 @@
+package euler;
+
+public class Problem_017 {
+
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+
+		System.out.println("There are " + countLetters(1000) + " letters when the numbers one through one thousand are written out in English.");
+
+		long endTime = System.currentTimeMillis();
+		System.out.println("It took " + (endTime - startTime) + " ms.");
+	}
+	
+	private static int countLetters(int n) {
+		int letters = 0;
+
+		for(int i =  1; i < n; i++) {
+			if(i < 21) {
+				letters += getLetters(i);
+			} else if(i < 100) {
+				letters += getLetters(i / 10 * 10);
+				letters += getLetters(i % 10);
+			} else {
+				letters += getLetters(i / 100) + 7;
+
+				if(i % 100 != 0) {
+					letters += 3; 		//covers the x-hundred AND x case
+					if(i % 100 < 21) {
+						letters += getLetters(i % 100);
+					} else {
+						letters += getLetters((i % 100) / 10 * 10);
+						letters += getLetters(i % 10);
+					}
+				} 	
+			}
+		}
+		
+		return letters + 11;
+	}
+
+	private static int getLetters(int number) {
+		switch(number) {
+			case 1:
+			case 2:
+			case 6:
+			case 10:
+				return 3;
+			case 4:
+			case 5:
+			case 9:
+				return 4;
+			case 3:
+			case 7:
+			case 8:
+			case 40:
+			case 50:
+			case 60:
+				return 5;
+			case 11:
+			case 12:
+			case 20:
+			case 30:
+			case 80:
+			case 90:
+				return 6;
+			case 15:
+			case 16:
+			case 70:
+				return 7;
+			case 13:
+			case 14:
+			case 18:
+			case 19:
+				return 8;
+			case 17:
+				return 9;
+			default:
+				return 0;
+		}
+	}
+
+}
