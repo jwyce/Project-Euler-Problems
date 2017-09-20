6a653e8eaba4109c31ce96378ed6cdf3bf57b663
Tom Orth <torth212@gmail.com>
2017-09-20 00:27:11 -0400

    fixed spacing
    That spacing really annoyed me for some reason lol

--- Problem_041.java
+++ Problem_041.java
@@ -50,10 +50,9 @@ public class Problem_041 {
 	
 	private static boolean isPrime(long num) {
 		if (num < 2) return false;
-        if (num == 2) return true;
-        if (num % 2 == 0) return false;
-        for (long i = 3; i * i <= num; i += 2)
-            if (num % i == 0) return false;
-        return true;
+                if (num == 2) return true;
+                if (num % 2 == 0) return false;
+                for (long i = 3; i * i <= num; i += 2) if (num % i == 0) return false;
+                return true;
 	}
 }
