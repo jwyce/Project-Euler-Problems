0577776b842d0c60148c7f9c04cd8dd67a0374c3
Jared Wyce <wycejared@gmail.com>
2018-03-13 16:16:29 -0400

    flawless fifty

---
+++ Problem_049.java
@@ -0,0 +1,52 @@
+package euler;
+
+import java.util.Arrays;
+
+public class Problem_049 {
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        System.out.println(primePerm());
+        
+        long endTime = System.currentTimeMillis();
+        System.out.println("It took " + (endTime - startTime) + " ms.");
+    }
+
+    private static String primePerm() {
+        String seq;
+        
+        for (int n = 1001; n < 10000; n += 2) {
+            seq = "";
+            if (isPrime(n)) {
+                if (n == 1487 || n == 4817 || n == 8157) continue;
+                seq += n;
+                for (int i = 1000; i < 3333; i++) {
+                    if (isPrime(i+n) && isPrime(2*i+n) && isPerm(Integer.toString(n).toCharArray(), Integer.toString(i+n).toCharArray())
+                            && isPerm(Integer.toString(n).toCharArray(), Integer.toString(2*i+n).toCharArray())) {
+                        seq += (i+n);
+                        seq += (2*i+n);
+                        return seq;
+                    }
+                }
+            }
+        }
+        
+        return "";
+    }
+    
+    private static boolean isPerm(char[] a, char[] b) {
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.toString(a).equals(Arrays.toString(b));
+    }
+    
+    private static boolean isPrime(long num) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (long i = 3; i * i <= num; i += 2)
+            if (num % i == 0) return false;
+        return true;
+    }
+}
---
+++ Problem_050.java
@@ -0,0 +1,51 @@
+package euler;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Problem_050 {
+
+    
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        System.out.println(consecPrimeSum(1000000));
+        
+        long endTime = System.currentTimeMillis();
+        System.out.println("It took " + (endTime - startTime) + " ms.");
+    }
+
+    private static String consecPrimeSum(int thresh) {
+        PriorityQueue<int[]> maxheap = new PriorityQueue<int[]>(1000, (a, b) -> b[0] - a[0]);
+        int sum = 2;
+        
+        for (int i = 3; i < thresh; i += 2) {
+            int prime, count = 1;
+            if (isPrime(sum) && isPrime(i)) prime = i;
+            else continue;
+            
+            while (sum + prime < thresh) {
+                sum += prime;
+                prime += 2;
+                while (!isPrime(prime)) prime += 2;
+                count++;
+                
+                int[] pair = { count, sum };
+                if (isPrime(sum)) maxheap.offer(pair);
+            }
+            
+            sum = i;
+        }
+        
+        return Arrays.toString(maxheap.poll());
+    }
+    
+    private static boolean isPrime(long num) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (long i = 3; i * i <= num; i += 2)
+            if (num % i == 0) return false;
+        return true;
+    }
+}
